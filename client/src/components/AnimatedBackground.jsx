870a6ddaafab8869f539db29c364147e6fa3b571
himdawwad <himd@focushimd.site>
2026-09-04 01:19:52 +0300

    chore: clean background to solid color only

--- client/src/components/AnimatedBackground.jsx
+++ client/src/components/AnimatedBackground.jsx
@@ -1,11 +1,5 @@
 const AnimatedBackground = () => {
-  return (
-    <div className="animated-bg" aria-hidden="true">
-      <div className="bg-orb orb-1" />
-      <div className="bg-orb orb-2" />
-      <div className="bg-orb orb-3" />
-    </div>
-  );
+  return null;
 };
 
 export default AnimatedBackground;
