b6f2ffea54303b06776df8775dac393798855f53
himdawwad <himd@focushimd.site>
2026-09-04 01:23:08 +0300

    chore: restore soft orbs background, keep boy image removed

--- client/src/components/AnimatedBackground.jsx
+++ client/src/components/AnimatedBackground.jsx
@@ -1,5 +1,11 @@
 const AnimatedBackground = () => {
-  return null;
+  return (
+    <div className="animated-bg" aria-hidden="true">
+      <div className="bg-orb orb-1" />
+      <div className="bg-orb orb-2" />
+      <div className="bg-orb orb-3" />
+    </div>
+  );
 };
 
 export default AnimatedBackground;
